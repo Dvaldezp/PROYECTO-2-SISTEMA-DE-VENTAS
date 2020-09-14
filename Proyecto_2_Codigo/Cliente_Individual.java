0751f6ae2ad091697d22782a991e18c226d5959c
LosChampions <dvaldezp@miumg.edu.gt>
2020-09-13 20:43:32 -0600

    Agregue 4 clase de codigo Cliente y main

---
+++ Proyecto_2_Codigo/Cliente_Individual.java
@@ -0,0 +1,24 @@
+package Proyecto_2_Codigo;
+
+public class Cliente_Individual extends Cliente{
+	
+	private String dpi;
+
+	
+
+	public Cliente_Individual(String nombreCliente, String apellidoCliente, String direccion, String dpi) {
+		
+		super(nombreCliente, apellidoCliente, direccion);
+		this.dpi=dpi;
+	}
+
+
+
+	
+	
+	
+	
+	
+	
+
+}
