bbe30a02d12a5164105d41c021d8eb905e323455
LosChampions <dvaldezp@miumg.edu.gt>
2020-09-13 22:52:06 -0600

    se modifico la clase hijas de Cliente

--- Proyecto_2_Codigo/Cliente_Individual.java
+++ Proyecto_2_Codigo/Cliente_Individual.java
@@ -1,24 +1,26 @@
 package Proyecto_2_Codigo;
 
-public class Cliente_Individual extends Cliente{
-	
+public class Cliente_Individual extends Cliente {
+
 	private String dpi;
 
-	
-
 	public Cliente_Individual(String nombreCliente, String apellidoCliente, String direccion, String dpi) {
-		
+
 		super(nombreCliente, apellidoCliente, direccion);
-		this.dpi=dpi;
+		this.dpi = dpi;
 	}
 
+	public String getDpi() {
+		return dpi;
+	}
 
+	public void setDpi(String dpi) {
+		this.dpi = dpi;
+	}
 
-	
-	
-	
-	
-	
-	
+	@Override
+	public String toString() {
+		return "Cliente_Individual [" + super.toString() + "dpi=" + dpi + "]";
+	}
 
 }
--- Proyecto_2_Codigo/MainMomentaneo.java
+++ Proyecto_2_Codigo/MainMomentaneo.java
@@ -14,10 +14,11 @@ public class MainMomentaneo {
 		
 		Cliente_Individual eCliente =new Cliente_Individual("Edy","Chay","Lote 40","556563566");
 		
-		Cliente_Individual eCliente1 =new Cliente_Individual("Edy","Chay","Lote 40","556563566");
+		Cliente_Empresa eCliente1 =new Cliente_Empresa("Edy","Chay","Lote 40","Juan",1);
 		
 		
-		System.out.println(eCliente.toString()+	eCliente1.toString());
+		System.out.println(eCliente.toString()+"\n"
+		+	eCliente1.toString());
 		
 		
 
