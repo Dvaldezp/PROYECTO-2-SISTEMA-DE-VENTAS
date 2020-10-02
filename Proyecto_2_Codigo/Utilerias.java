572dc535647af4fe140bd746123010d343c8a63f
LosChampions <dvaldezp@miumg.edu.gt>
2020-10-02 02:11:32 -0600

    prueba

--- Proyecto_2_Codigo/Utilerias.java
+++ Proyecto_2_Codigo/Utilerias.java
@@ -1,21 +1,7 @@
 package Proyecto_2_Codigo;
 
-
-/**
- * Esta clase es para devolver el nombre de la clase de forma especifica 
- * @author Edy Chay
- * @author Daniel Valdez
- *
- */
 public class Utilerias {
 
-	/**
-	 * Variable, regresa el parametro
-	 * @param c
-	 * @return
-	 */
-		
-	
 	public static String getNombreClase(Class c) {
 		return c.getSimpleName().toString();
 		
